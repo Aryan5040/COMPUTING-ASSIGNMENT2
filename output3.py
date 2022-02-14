0c020c4b9889bcbf909bfae703b6f6818705d2ad
Aryan5040 <97087886+Aryan5040@users.noreply.github.com>
2022-02-14 18:48:50 +0530

    Add files via upload

---
+++ output3.py
@@ -0,0 +1,119 @@
+Python 3.10.0 (tags/v3.10.0:b494f59, Oct  4 2021, 19:00:18) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:/Users/singh/AppData/Local/Programs/Python/Python310/asgn3 21105040.py
+Question Number 1 
+
+Enter the String :
+My name is Aryan Singh I love my name aryan singh
+Hence the repititions of Words can be given as :
+ {'my': 2, 'name': 2, 'is': 1, 'aryan': 2, 'singh': 2, 'i': 1, 'love': 1} 
+
+Question 2 
+
+Enter year :2023
+Enter month :2
+Enter day: 27
+Next date in the format(dd/mm/yyyy) is: 28/2/2023 
+ 
+Question 3 
+
+Number of Terms to be entered in list :
+5
+Enter the required element :
+1
+Enter the required element :
+2
+Enter the required element :
+4
+Enter the required element :
+5
+Enter the required element :
+6
+The required list is:
+ [(1, 1), (2, 4), (4, 16), (5, 25), (6, 36)]
+Question  4 
+
+Enter your grade point: 8
+Your Grade is 'B+' and Very Good performance 
+
+Question 5 
+
+ ABCDEFGHIJK
+  ABCDEFGHI
+   ABCDEFG
+    ABCDE
+     ABC
+      A
+
+
+Question 6 Making a Dictionary 
+
+Enter name: 
+Aryan
+Enter SID: 
+21105040
+{'Aryan': 21105040}
+Want to make another entry? 
+ Press 'Y' for YES / 'N' for NO.
+y
+Enter name: 
+sumit
+Enter SID: 
+21104012
+{'Aryan': 21105040, 'sumit': 21104012}
+Want to make another entry? 
+ Press 'Y' for YES / 'N' for NO.
+y
+Enter name: 
+Ayush
+Enter SID: 
+21105020
+{'Aryan': 21105040, 'sumit': 21104012, 'Ayush': 21105020}
+Want to make another entry? 
+ Press 'Y' for YES / 'N' for NO.
+y
+Enter name: 
+chirag
+Enter SID: 
+21105037
+{'Aryan': 21105040, 'sumit': 21104012, 'Ayush': 21105020, 'chirag': 21105037}
+Want to make another entry? 
+ Press 'Y' for YES / 'N' for NO.
+n
+(a) The Stored Student Details are : 
+ {'Aryan': 21105040, 'sumit': 21104012, 'Ayush': 21105020, 'chirag': 21105037} 
+
+(b) Dictionary sorted on the basis of names: 
+ {'Aryan': 21105040, 'Ayush': 21105020, 'chirag': 21105037, 'sumit': 21104012} 
+
+(c) Dictionary sorted on the basis of SID: 
+ {'sumit': 21104012, 'Ayush': 21105020, 'chirag': 21105037, 'Aryan': 21105040} 
+
+(d) Enter a SID to get the Student's name :21105040
+Name related to following sid is: 
+ Aryan 
+
+Question 7 
+
+How many terms are to be printed in fibonacci series?
+8
+The Required Fibonacci series is:
+ [0, 1, 1, 2, 3, 5, 8, 13] 
+
+Average of following series is:
+ 4.125 
+
+Question 8
+
+(a) {1, 3, 5, 6, 8} 
+
+(b) {17, 3, 6, 8, 9, 13} 
+
+(c) {1, 2, 4, 5} 
+
+d) {6, 7, 8, 9, 10} 
+
+e) {10, 7} 
+
+Thank You
