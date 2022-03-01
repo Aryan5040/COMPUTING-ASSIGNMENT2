cfe0611fa8c7e4280cae5e4b5c338cbb486062f8
Aryan5040 <97087886+Aryan5040@users.noreply.github.com>
2022-03-01 22:46:36 +0530

    Add files via upload

---
+++ output4.py
@@ -0,0 +1,74 @@
+Python 3.10.0 (tags/v3.10.0:b494f59, Oct  4 2021, 19:00:18) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:/Users/singh/AppData/Local/Programs/Python/Python310/assignment4.py
+Question Number 1 
+
+
+Move Disk 1 from A to C
+Move Disk 2 from A to B
+Move Disk 1 from C to B
+Move Disk 3 from A to C
+Move Disk 1 from B to A
+Move Disk 2 from B to C
+Move Disk 1 from A to C
+
+
+Question Number 2 
+
+Enter the number of rows in Pascal's Triangle: 4
+
+ Triangle using recursions :
+
+      1   
+    1   1   
+  1   2   1   
+1   3   3   1   
+
+ Using loops: 
+
+      1   
+    1   1   
+  1   2   1   
+1   3   3   1   
+
+
+Question Number 3 
+
+Enter the first integer: 2
+Enter the Second integer: 6
+Remainder is:  2
+Quotient is:  0
+One value is zero
+{6}
+{6, 7}
+{5, 6, 7}
+{8, 5, 6, 7}
+{8, 5, 6, 7}
+{8, 5, 6, 7}
+Immutable set:  frozenset({8, 5, 6, 7})
+Largest value in the set:  8
+Hash value:  8
+
+
+Question Number 4: 
+
+Object created
+The name of the Student is Sahil Sharma and SID is 21104019.
+Object destroyed
+
+
+Question Number 5
+(a) The updated salary of the employee Mehak is 70000
+(b) Employee Viren record deleted
+
+
+Question Number 6 
+
+Enter the first word: Aryan
+
+ Enter a new meaningful word to test your friendship: nayra
+The letters aren't exact, friendship is fake!!
+
+
+Thank You
